54702420b5503ca907ec0047c86f0e7c8749b301
Muhammad Maaz Khan <55675110+Maazito317@users.noreply.github.com>
2020-01-03 00:34:43 -0600

    Question 2

--- AlgorithmsI/src/Algorithms_Analysis_Interview/BitonicSearchArray.java
+++ AlgorithmsI/src/Algorithms_Analysis_Interview/BitonicSearchArray.java
@@ -1,8 +1,24 @@
 package Algorithms_Analysis_Interview;
 
+/**
+ * Search in a bitonic array. An array is bitonic if it is comprised of an increasing sequence of integers followed immediately by a decreasing sequence of integers. Write a program that, given a bitonic array of nn distinct integer values, determines whether a given integer is in the array.
+ *
+ * Standard version: Use ∼3lgn compares in the worst case.
+ * Signing bonus: Use ∼2lgn compares in the worst case (and prove that no algorithm can guarantee to perform fewer than ∼2lgn compares in the worst case).
+ */
 public class BitonicSearchArray {
 
-    private static int findMax(int[] arr, int start, int end){ //using binary search
+    public static boolean SearchVal(int[] arr, int start, int end, int toFind, boolean asc){
+        int mid = (start+end)/2;
+        if (start == end) return true;
+        if (start > end) return false;
+
+        if ((asc && arr[mid] > toFind )||(!asc && arr[mid] < toFind)) return SearchVal(arr, start,mid-1,toFind,asc);
+        else return SearchVal(arr,mid+1,end,toFind,asc);
+
+    }
+
+    public static int findMax(int[] arr, int start, int end){ //using binary search
         int mid = (start + end)/2;
         if (start == end) return mid;
 
@@ -13,6 +29,18 @@ public class BitonicSearchArray {
         }
     }
 
+    public static boolean find(int[] arr,int peak,int toFind){
+        return SearchVal(arr,0,peak, toFind,true) || SearchVal(arr,peak,arr.length-1,toFind,false);
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,3,5,7,9,11,8,6,4,2};
+        int toFind = 7;
+
+        int max = findMax(arr,0,arr.length-1);
+        System.out.println(toFind+":"+find(arr,max,toFind));
+
+    }
 
 }
 
--- AlgorithmsI/src/Algorithms_Analysis_Interview/ThreeSum.java
+++ AlgorithmsI/src/Algorithms_Analysis_Interview/ThreeSum.java
@@ -4,6 +4,11 @@ import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 3-SUM in quadratic time. Design an algorithm for the 3-SUM problem that takes time proportional to n^2
+ *   in the worst case. You may assume that you can sort the nn integers in time proportional to n^2
+ *  or better.
+ */
 public class ThreeSum {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
