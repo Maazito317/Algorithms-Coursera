17b6db7d6382a39bc4d63196433b2774f178cebc
Muhammad Maaz Khan <55675110+Maazito317@users.noreply.github.com>
2020-01-10 09:53:57 -0600

    Q1

---
+++ AlgorithmsI/src/Sorting_Interview/IntersectionCheck.java
@@ -0,0 +1,44 @@
+package Sorting_Interview;
+
+public class IntersectionCheck {
+    static class Point implements Comparable<Point>{
+        private int x;
+        private int y;
+
+        public Point(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+        @Override
+        public int compareTo(Point o) { // true if o is smaller than this
+            if (o.x > this.x) return -1;
+            if (o.x < this.x) return 1;
+            if (o.y > this.y) return -1;
+            if (o.y < this.y) return 1;
+            return 0;
+        }
+    }
+    public static int count(Point[] first, Point[] second) {
+        int count = 0;
+        ShellSort.sort(first);
+        ShellSort.sort(second);
+        int i = 0;
+        int j = 0;
+
+        while (i < first.length && j < second.length) {
+            if (first[i].compareTo(second[j]) == 0) {
+                i++;
+                j++;
+                count++;
+            } else if (first[i].compareTo(second[j]) < 0) i++;
+            else j++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        Point[] first = {new Point(1,2), new Point(5,3), new Point(2,2), new Point(7,3),new Point(1,9)};
+        Point[] second = {new Point(8,2), new Point(5,3), new Point(4,7), new Point(7,3),new Point(0,9)};
+        System.out.println("Num Intersection: "+ count(first,second));
+    }
+}
